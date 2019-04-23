28452240466f7c1619b4009d2f6a66b40ac27386
qingfeng <1737543007@qq.com>
2019-04-23 18:55:14 +0800

    公共项目

---
+++ breeze-api/src/test/java/com/qingfeng/breeze/api/BreezeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qingfeng.breeze.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreezeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- breeze-log/src/main/java/com/qingfeng/breeze/log/Application.java
+++ breeze-log/src/main/java/com/qingfeng/breeze/log/Application.java
@@ -1,5 +1,6 @@
 package com.qingfeng.breeze.log;
 
+import com.qingfeng.breeze.api.util.R;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
