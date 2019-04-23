618000340d88d92dffdf964a3712013f90c9e0d4
qingfeng <1737543007@qq.com>
2019-04-23 18:20:33 +0800

    添加配置中心

---
+++ breeze-mongodb/src/main/java/com/qingfeng/breeze/mongodb/BreezeMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.qingfeng.breeze.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreezeMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreezeMongodbApplication.class, args);
+	}
+
+}
