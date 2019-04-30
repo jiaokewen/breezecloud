d2f77fe6e637a3ce5fddb62895054bca8adc4379
qingfeng <1737543007@qq.com>
2019-04-30 15:16:01 +0800

    网关中心

---
+++ breeze-gateway/src/test/java/com/qingfeng/breeze/gateway/BreezeGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qingfeng.breeze.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreezeGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
