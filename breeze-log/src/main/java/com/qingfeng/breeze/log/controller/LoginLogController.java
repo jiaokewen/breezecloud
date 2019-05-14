18551f14838c82a753efe2e9b721b5a97c2add1c
qingfeng <1737543007@qq.com>
2019-05-14 17:23:33 +0800

    :)

--- breeze-log/src/main/java/com/qingfeng/breeze/log/controller/LoginLogController.java
+++ breeze-log/src/main/java/com/qingfeng/breeze/log/controller/LoginLogController.java
@@ -11,4 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/loginLog/")
 public class LoginLogController extends BaseController<LoginLog> {
 
+    public static int strStr(String haystack, String needle) {
+        if (needle == null || needle.equals("")) {
+            return 0;
+        }
+        if (haystack == null || haystack.equals("")) {
+            return -1;
+        }
+        int n = needle.length();
+        loop: for (int i=0; i <=haystack.length()-n;i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                for (int j=1;j<n;j++) {
+                    if (haystack.charAt(i+j) != needle.charAt(j)) {
+                        continue loop;
+                    }
+                }
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi","asssasd"));
+    }
 }
