1ff36fd49778def81d9f96564dd90e4f64270280
qingfeng <1737543007@qq.com>
2019-05-15 17:40:08 +0800

    :see_no_evil:

--- breeze-auth/src/main/java/com/qingfeng/breeze/auth/Application.java
+++ breeze-auth/src/main/java/com/qingfeng/breeze/auth/Application.java
@@ -2,12 +2,39 @@ package com.qingfeng.breeze.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+public class Application extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication()
+				.withUser("guest").password("guest").authorities("WRIGTH_READ")
+				.and()
+				.withUser("admin").password("admin").authorities("WRIGTH_READ", "WRIGTH_WRITE");
+	}
+
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
 }
