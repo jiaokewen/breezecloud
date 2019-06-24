77cdfcb97897ab9ec6a8cfd3e9ef75ef29f45630
jiaokewen <1737543007@qq.com>
2019-06-24 11:50:35 +0800

    哦哦哦

---
+++ breeze-zipkin/src/main/java/com/qingfeng/breeze/zipkin/Application.java
@@ -0,0 +1,15 @@
+package com.qingfeng.breeze.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
